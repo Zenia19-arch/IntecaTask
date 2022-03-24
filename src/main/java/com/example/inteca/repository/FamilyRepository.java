bd1a4d69104a1d626d119292d04bc71e6535210b
Yauheni Bartoshyk <bartoshik2000@mail.ru>
2022-03-24 19:50:33 +0100

    add repository

--- src/main/java/com/example/inteca/repository/FamilyRepository.java
+++ src/main/java/com/example/inteca/repository/FamilyRepository.java
@@ -4,4 +4,5 @@ import com.example.inteca.domain.Family;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FamilyRepository extends JpaRepository<Family, Long> {
+
 }
